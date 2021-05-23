647db4094f9754e23ceedf5a00c8d05ea852da46
Kihara, Takuya <gray@tacck.net>
2021-05-23 17:52:24 +0900

    fix coding styles

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false;
+import '@aws-amplify/ui-vue'
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   vuetify,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
